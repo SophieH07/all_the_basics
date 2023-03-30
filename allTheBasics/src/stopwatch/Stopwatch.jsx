298dd8d9e1628adf60768b78734d97600aad019e
sophie <szaniszlozs7@gmail.com>
2023-03-30 13:00:51 -0500

    start stopwatch

--- allTheBasics/src/stopwatch/Stopwatch.jsx
+++ allTheBasics/src/stopwatch/Stopwatch.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react";
+
 const Stopwatch = () => {
-  return <div>Hi</div>;
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  let interval;
+
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="minutes">Minutes:</label>
+        <input type="number" />
+        <label htmlFor="seconds">Seconds:</label>
+        <input type="number" />
+        <button type="submit">Start</button>
+      </form>
+    </div>
+  );
 };
 
 export default Stopwatch;
